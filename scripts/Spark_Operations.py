e9a9bc0a96917414b763e85d2762f07daa29af44
fmah922-code <faiselmahmud6@gmail.com>
2025-04-04 21:56:23 -0700

    removing settings that are found in agriflow.py

--- scripts/Spark_Operations.py
+++ scripts/Spark_Operations.py
@@ -1,20 +1,3 @@
-import os
-import sys
-from pyspark.sql import SparkSession
-from pymongo.mongo_client import MongoClient
-
-config_path = os.getcwd().replace('\\scripts','')
-sys.path.insert(0, config_path)
-
-from config import settings 
-
-spark = SparkSession.builder \
-        .config("spark.jars", "C:\jdbc\postgresql-42.7.5.jar") \
-        .getOrCreate()
-
-
-mongo_conn = MongoClient(settings.mongo_client)[settings.mongo_default_db]
-
 def stringify_id(row):
     return str(row['_id'])
 
